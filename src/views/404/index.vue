34a0e1ff22dc01d3407fa0a80a7a4a13975bf3f8
maoPartTime <96091665+1105931818@users.noreply.github.com>
2023-08-24 15:19:58 +0800

    Add files via upload

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,7 +1,12 @@
 <template>
     <h1>404</h1>
+    <Identify @updateIdentifyCode="setIdentifyCode" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const setIdentifyCode = (value: string) => {
+    console.log(value)
+}
+</script>
 
 <style scoped lang="scss"></style>
